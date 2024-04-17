0582f42de6812dc2d45a3f1e3a2f2170a037d168
Akashgola123 <golaakash0@gmail.com>
2024-04-17 11:29:43 +0530

    stage 2 prepare  base model  complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from chickenclassfier import logger
 from chickenclassfier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from chickenclassfier.pipeline.stage_02_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -12,3 +13,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
