931364890c025c400523211115e6ade244d596da
Akashgola123 <golaakash0@gmail.com>
2024-04-18 00:13:01 +0530

    callback commit succesfully

--- src/chickenclassfier/config/configuration.py
+++ src/chickenclassfier/config/configuration.py
@@ -1,7 +1,9 @@
+import os
 from chickenclassfier.constants import *
 from chickenclassfier.utils.common import read_yaml, create_directories
 from chickenclassfier.entity.config_entity import DataIngestionConfig
 from chickenclassfier.entity.config_entity import PrepareBaseModelConfig
+from chickenclassfier.entity.config_entity import PrepareCallbacksConfig
 
 class ConfigurationManager:
     def __init__(
@@ -47,3 +49,23 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+    
+    
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        # model_ckpt_dir = Path(config.checkpoint_model_filepath).parent
+
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
